d27a9f116c42b10d0b61b9ac0f49f172dd992fbc
Dante1404 <108441394+Dante1404@users.noreply.github.com>
2025-02-06 00:59:47 +0700

    Delete ProjectEXE202-code directory

--- ProjectEXE202-code/src/routes/site.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const siteControllers = require('../app/controllers/SiteControllers');
-
-router.get('/search', siteControllers.search);
-router.get('/', siteControllers.index);
-
-module.exports = router;
--- ProjectEXE202-code/src/until/mongoose.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    mutipleMongoose: function (mongooses) {
-        return mongooses.map((mongoose) => mongoose.toObject());
-    },
-
-    simpleMongoose: function (mongoose) {
-        return mongoose ? mongoose.toObject() : mongoose;
-    },
-};
